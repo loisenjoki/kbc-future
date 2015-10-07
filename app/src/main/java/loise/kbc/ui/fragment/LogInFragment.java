1e045e864b72a95449732440cdd265423c150e01
Faith Muli <faithmuli33@gmail.com>
2015-10-07 09:16:10 +0300

    twitter login working but looping back

--- app/src/main/java/loise/kbc/ui/fragment/LogInFragment.java
+++ app/src/main/java/loise/kbc/ui/fragment/LogInFragment.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
@@ -24,8 +26,8 @@ import loise.kbc.navigationviewpagerliveo.R;
 
 
 public class LogInFragment extends android.support.v4.app.Fragment {
-    private static final String TWITTER_KEY = "CtXW8KLLGFfQTqWlrdoK7oUbR";
-    private static final String TWITTER_SECRET = "xq0mHEOlOpBVfr1uyvehFF1dt05YIAPxCHav0diBfr99QuBXrL";
+   // private static final String TWITTER_KEY = "CtXW8KLLGFfQTqWlrdoK7oUbR";
+   // private static final String TWITTER_SECRET = "xq0mHEOlOpBVfr1uyvehFF1dt05YIAPxCHav0diBfr99QuBXrL";
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
 
    private TwitterLoginButton loginButton;
@@ -35,42 +37,48 @@ public class LogInFragment extends android.support.v4.app.Fragment {
 
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_log_in, container, false);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(getActivity(), new Twitter(authConfig));
-       loginButton = (TwitterLoginButton) v.findViewById(R.id.login_button1);
-        loginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // Do something with result, which provides a TwitterSession for making API calls
-            }
+       // TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        //Fabric.with(getActivity(), new Twitter(authConfig));
 
-            @Override
-            public void failure(TwitterException exception) {
-                // Do something on failure
-            }
+
+       loginButton = (TwitterLoginButton) v.findViewById(R.id.login_button1);
+        loginButton.setCallback(new LogInHandler() {
+
         });
-        return v;
+
+            return v;
 
 
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
         loginButton.onActivityResult(requestCode, resultCode, data);
     }
-    @Override
-    public void onResume() {
-        super.onResume();
+
+
+    private class LogInHandler extends Callback<TwitterSession> {
+        @Override
+        public void success(Result<TwitterSession> result) {
+            // The TwitterSession is also available through:
+            // Twitter.getInstance().core.getSessionManager().getActiveSession()
+            TwitterSession session = result.data;
+            // TODO: Remove toast and use the TwitterSession's userID
+            // with your app's user model
+            String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+            Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        public void failure(TwitterException e) {
+            Log.d("TwitterKit", "Login with Twitter failure");
+
+        }
+
     }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-
-
-
-
+    
 
 }
--- app/src/main/java/loise/liveo/ui/activity/TwitterApplication.java
+++ app/src/main/java/loise/liveo/ui/activity/TwitterApplication.java
@@ -1,5 +1,6 @@
 package loise.liveo.ui.activity;
 
+import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,12 +11,20 @@ import io.fabric.sdk.android.Fabric;
 import loise.kbc.navigationviewpagerliveo.R;
 
 
-public class TwitterApplication extends AppCompatActivity {
+public class TwitterApplication extends Application {
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "CtXW8KLLGFfQTqWlrdoK7oUbR";
     private static final String TWITTER_SECRET = "xq0mHEOlOpBVfr1uyvehFF1dt05YIAPxCHav0diBfr99QuBXrL";
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
@@ -23,10 +32,7 @@ public class TwitterApplication extends AppCompatActivity {
         setContentView(R.layout.activity_timeline_kbc);
 
 
-    }
+    }*/
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+
 }
